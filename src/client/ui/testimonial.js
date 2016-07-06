453c92732967f94e2cc321bfce7e9a6cd6263c39
Chris Bergstrom <chris@snapcare.io>
2016-07-06 11:18:09 -0700

    small change

--- src/client/ui/testimonial.js
+++ src/client/ui/testimonial.js
@@ -74,15 +74,6 @@ export default React.createClass({
                     </div>
                   </blockquote>
                 </div>
-                <div className="testimonialGroup">
-                  <blockquote ClassName="testimonialInfo">Bacon ipsum dolor amet salami ball tip kevin pastrami chuck shank jowl prosciutto boudin leberkas brisket. Chicken spare ribs beef ribs, pig kevin tongue flank
-                    salami pork belly pork kielbasa shank.
-                    <div className="testimonialCard">
-                      <cite className="testimonialName">David Carley</cite>
-                      <div className="testimonialContact">Captian pc</div>
-                    </div>
-                  </blockquote>
-                </div>
               </div>
             </div>
         )
